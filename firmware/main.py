5ae9104268fb6e519c81933e81bac0982a3246cd
Thomas-Jld <thomas.juldo@edu.devinci.fr>
2023-01-23 10:34:17 +0100

    Refactor firmware for lights module and touch threshold
    - Replace `NeoPixel` with local `lights` module
    - Adjust `set_brightness` and `TOUCH_THRESHOLD`
    
    [firmware/main.py]
    - Replace `NeoPixel` with `lights` module
    - Adjust `set_brightness` to work with `lights` module
    - Adjust `TOUCH_THRESHOLD` to `250`

--- firmware/main.py
+++ firmware/main.py
@@ -1,24 +1,12 @@
 from machine import Pin, TouchPad
-from neopixel import NeoPixel
+from lights import animate, set_brightness, np
 from time import sleep
-from math import sin
 
-np = NeoPixel(Pin(14), 128)
 touch = TouchPad(Pin(4))
 
 PI = 3.1415
 ANIM_FRAME = 0
 TOUCH_THRESHOLD = 250
-
-def set_brightness(np: NeoPixel, brightness: float):
-    for i in range(np.n):
-        np[i] = (int(np[i][0] * brightness), int(np[i][1] * brightness), int(np[i][2] * brightness))
-    np.write()
-
-def animate(np: NeoPixel, frame: int):
-    for i in range(np.n):
-        val = min(max(sin((i + frame) * 2 * PI / np.n) + 0.5, 0),1)
-        np[i] = (int(val * 255 * i / np.n), 0, int(val * 255 * (1 - i / np.n)))
 
 
 while True:
